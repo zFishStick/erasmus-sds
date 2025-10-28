9640cd99dbd2190289b2b56c42ce5cc3725dd1d1
zFishStick <gabri.pesce0@gmail.com>
2025-10-28 20:00:47 +0100

    Structure changes and filter feature

--- Project/sds2/src/main/java/com/sds2/Server.java
+++ Project/sds2/src/main/java/com/sds2/Server.java
@@ -1,16 +1,30 @@
 package com.sds2;
 
-import com.amadeus.exceptions.ResponseException;
 import java.io.IOException;
+import java.net.URI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.Desktop;
+
 @SpringBootApplication
 public class Server {
-    public static void main(String[] args) throws IOException, ResponseException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(Server.class, args);
         System.out.println("Server in esecuzione su http://localhost:8080");
+        System.setProperty("java.awt.headless", "false");
+        
+        try {
+            URI uri = new URI("http://localhost:8080");
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(uri);
+            } else {
+                System.out.println("Apertura automatica del browser non supportata.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
+
