bdfad493bfcd1ddc8ec7e806bf167d4c3a80bb61
zFishStick <gabri.pesce0@gmail.com>
2025-11-11 23:26:36 +0100

    Completed some tests

--- Project/sds2/src/test/java/com/sds2/classes/PriceTest.java
+++ Project/sds2/src/test/java/com/sds2/classes/PriceTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-public class PriceTest {
+class PriceTest {
 
     @Test
     void gettersSettersAndConstructor() {
