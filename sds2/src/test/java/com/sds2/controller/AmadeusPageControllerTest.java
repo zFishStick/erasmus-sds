dde6d274457c10a5b6a99c8f7899aac1d6b9659d
zFishStick <gabri.pesce0@gmail.com>
2025-12-09 16:28:36 +0100

    Add ActiveProfiles annotation to PageServerTest for test environment configuration

--- sds2/src/test/java/com/sds2/controller/AmadeusPageControllerTest.java
+++ sds2/src/test/java/com/sds2/controller/AmadeusPageControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AmadeusPageControllerTest {
 
 	@Autowired
