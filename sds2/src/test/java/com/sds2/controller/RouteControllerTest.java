3fc666433e0c766d5e787c9267f9f1f494c99abd
zFishStick <gabri.pesce0@gmail.com>
2025-12-09 16:28:36 +0100

    Add ActiveProfiles annotation to PageServerTest for test environment configuration

--- sds2/src/test/java/com/sds2/controller/RouteControllerTest.java
+++ sds2/src/test/java/com/sds2/controller/RouteControllerTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.sds2.classes.CitySummary;
 import com.sds2.classes.Places;
@@ -28,6 +29,7 @@ import jakarta.servlet.http.HttpSession;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class RouteControllerTest {
 
     @Mock
